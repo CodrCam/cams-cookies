3ca8bab034f467509e35a6327283db43fdc64035
Cameron Griffin <cambackcountry@gmail.com>
2023-02-16 18:28:41 -0800

    add branch

---
+++ test.js
@@ -0,0 +1,43 @@
+//Function to call up the literas
+/* function processCities(cities) {
+  for (let i = 0; i < cities.length; i++) {
+    let city = cities[i];
+    let store = new Store(city.name, city.minCustomers, city.maxCustomers, city.avgCookies);
+    store.simulateSales();
+    store.render();
+  }
+}
+
+let cities = [
+  { name: "Seattle", minCustomers: 23, maxCustomers: 65, avgCookies: 6.3 },
+  { name: "Tokyo", minCustomers: 3, maxCustomers: 24, avgCookies: 1.2 },
+  { name: "Dubai", minCustomers: 11, maxCustomers: 38, avgCookies: 3.7 },
+  { name: "Paris", minCustomers: 20, maxCustomers: 38, avgCookies: 2.3 },
+  { name: "Lima", minCustomers: 2, maxCustomers: 16, avgCookies: 4.6 }
+];
+ */
+
+
+//Create unlimited cities into page without function
+
+/* let Seattle = new Store('Seattle', 23, 65, 6.3);
+Seattle.simulateSales();
+Seattle.render();
+
+
+let Tokyo = new Store('Tokyo', 3, 24, 1.2);
+Tokyo.simulateSales();
+Tokyo.render();
+
+let Dubai = new Store('Dubai', 11, 38, 3.7);
+Dubai.simulateSales();
+Dubai.render();
+
+let Paris = new Store('Paris', 20, 38, 2.3);
+Paris.simulateSales();
+Paris.render();
+
+let Lima = new Store('Lima', 2, 16, 4.6);
+Lima.simulateSales();
+Lima.render();
+ */
